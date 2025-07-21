d474665d2881a51f7af98fe823b8b5fd8b9cdbdd
ynnekF <kennybf007@gmail.com>
2025-07-21 16:17:43 -0600

    shfmt aptmgr

--- scripts/aptmgr.sh
+++ scripts/aptmgr.sh
@@ -18,29 +18,27 @@ readonly __COLOR_GREEN='\033[0;32m'
 readonly __COLOR_YELLOW='\033[1;33m'
 readonly __COLOR_RESET='\033[0m'
 
-
 function show_upgradable() {
-    echo -e "\n${__COLOR_YELLOW}Upgradable packages:${__COLOR_RESET}"
-    apt list --upgradable 2>/dev/null | grep -v 'Listing...' | sort
+	echo -e "\n${__COLOR_YELLOW}Upgradable packages:${__COLOR_RESET}"
+	apt list --upgradable 2>/dev/null | grep -v 'Listing...' | sort
 }
 
-
 function show_installed() {
-    echo -e "\n${__COLOR_YELLOW}Installed packages:${__COLOR_RESET}"
-    apt list --installed | grep -Ff <(apt-mark showmanual) | sort | uniq
+	echo -e "\n${__COLOR_YELLOW}Installed packages:${__COLOR_RESET}"
+	apt list --installed | grep -Ff <(apt-mark showmanual) | sort | uniq
 }
 
 function show_all() {
-    echo -e "\n${__COLOR_YELLOW}All installed packages:${__COLOR_RESET}"
-    # View packages explicitly installed by the user
-    apt-mark showmanual | sort | uniq | while read -r package; do
-        # Check if the package is installed
-        if dpkg -l | grep -q "^ii\s\+$package"; then
-            echo "$package"
-        else
-            echo "Package $package is not installed."
-        fi
-    done
+	echo -e "\n${__COLOR_YELLOW}All installed packages:${__COLOR_RESET}"
+	# View packages explicitly installed by the user
+	apt-mark showmanual | sort | uniq | while read -r package; do
+		# Check if the package is installed
+		if dpkg -l | grep -q "^ii\s\+$package"; then
+			echo "$package"
+		else
+			echo "Package $package is not installed."
+		fi
+	done
 }
 
 ##
@@ -54,7 +52,7 @@ function main() {
 				show_upgradable
 				;;
 			--installed | -i)
-                show_installed
+				show_installed
 				;;
 			--all | -a)
 				show_all
